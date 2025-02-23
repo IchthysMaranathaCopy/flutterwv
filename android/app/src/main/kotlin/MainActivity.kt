1fa781a3958f65a1c4d52289ee5387c93afe2711
IchthysMaranathaCopy <servantofchristjesus@yahoo.com>
2025-02-23 09:43:29 +0530

    Delete android/app/src/main/kotlin directory

--- android/app/src/main/kotlin/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-import android.os.Bundle
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // No special WebView configuration needed here
-    }
-}
