30748695756ad3c102206a199916ec0fe01e8348
IchthysMaranathaCopy <servantofchristjesus@yahoo.com>
2025-02-23 08:30:06 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/MainActivity.kt
+++ android/app/src/main/kotlin/MainActivity.kt
@@ -1,14 +1,9 @@
-import android.webkit.ValueCallback
-import android.webkit.WebChromeClient
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        WebViewPlatform.instance.registerWith(
-            flutterEngine.dartExecutor.binaryMessenger,
-            WebViewFactory(this, flutterEngine.dartExecutor)
-        )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // No special WebView configuration needed here
     }
 }
